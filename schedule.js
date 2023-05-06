7cd1932efad02be7f7073ec6845aa2f586a91557
nass <cabliveer@gmail.com>
2023-05-06 18:22:00 +0200

    Solve linter error

--- schedule.js
+++ schedule.js
@@ -1,25 +1,23 @@
 window.onload = () => {
-     
-    const contents = document.querySelectorAll('.anim');
-    const options = {
-      root: null,
-      threshold: 0,
-      rootMargin: '-150px',
-    };
-  
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('section-animation');
-          observer.unobserve(entry.target);
-          return;
-        }
-        entry.target.classList.remove('section-animation');
-      });
-    }, options);
-  
-    contents.forEach((content) => {
-      observer.observe(content);
-    });
+  const contents = document.querySelectorAll('.anim');
+  const options = {
+    root: null,
+    threshold: 0,
+    rootMargin: '-150px',
   };
-  
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('section-animation');
+        observer.unobserve(entry.target);
+        return;
+      }
+      entry.target.classList.remove('section-animation');
+    });
+  }, options);
+
+  contents.forEach((content) => {
+    observer.observe(content);
+  });
+};
